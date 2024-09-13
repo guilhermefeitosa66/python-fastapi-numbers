d3f355bf56b6ca81594f3dd4b81d4f0f6b617bb6
Guilherme Feitoza <guilhermefeitosa66@gmail.com>
2024-09-13 18:55:56 -0300

    chore: include numbers endpoint routes

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 from fastapi import FastAPI, Request, status
 from fastapi.responses import JSONResponse
 
+from src.packages.numbers.controllers.numbers_controller import NumbersController
+
 
 app = FastAPI()
 
 @app.get("/")
 def health_check():
     return JSONResponse({"message": "OK"}, status_code=status.HTTP_200_OK)
+
+app.include_router(
+    router=NumbersController().router,
+    tags=["numbers"],
+    prefix="/api/v1/numbers"
+)
 
 @app.exception_handler(Exception)
 def default_exception_handler(_: Request, exc: Exception):
